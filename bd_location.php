3b705969102d89551a52de718b07c24c72ebf8c0
Md Shariar Sarkar <shariarbd@users.noreply.github.com>
2021-08-31 00:07:23 +0600

    location added

--- bd_location.php
+++ bd_location.php
@@ -1,1 +1,12 @@
+<?php 
 
+//Array of All District of Bangladesh 
+$bd_location = [
+    'barisāl' => explode(', ', 'barisal, bhola, jhalokati, patuakhali, pirojpur'),
+    'chittagong' => explode(', ', 'bandarban, brahmanbaria, chandpur, chittagong, comilla, cox\'s bazar, feni, khagrachhari, lakshmipur, noakhali, rangamati'),
+    'dhaka' => explode(', ', 'dhaka, gazipur, narsingdi, manikganj, munshiganj, narayanganj, mymensingh, sherpur, jamalpur, netrokona, kishoreganj, tangail, faridpur, maradipur, shariatpur, rajbari, gopalganj'),
+    'khulna' => explode(', ', 'bagerhat, chuadanga, jessore, jhenaidaha, khulna, kushtia, magura, meherpur, narail, satkhira'),
+    'rajshahi' => explode(', ', 'bogra, joypurhat, naogaon, natore, nawabganj, pabna, rajshahi, sirajganj'),
+    'rangpur' => explode(', ', 'dinajpur, gaibandha, kurigram, lalmonirhat, nilphamari, panchagarh, rangpur, thakurgaon'),
+    'sylhet' => explode(', ', 'habiganj, maulvi bazar, sunamganj, sylhet'),
+];
